f5509d951dda954b675c801187a58ed898f65bc6
Maheshkumarcseb <141344157+Maheshkumarcseb@users.noreply.github.com>
2023-08-06 12:39:45 +0530

    Create passbyreference.cpp

---
+++ passbyreference.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+ using namespace std;
+ void exchange(int &m,int &n);
+ int main()
+ {
+     int a,b;
+     cout<<"enter the value of a and b before swapping"<<endl;
+     cin>>a>>b;
+     cout<< "value of a and b before swapping";
+     cout <<a<<endl<<b<<endl;
+     exchange(a,b);
+     cout<< "value of a and b after swapping";
+     cout<<a<<endl<<b<<endl;
+     return 0;
+ }
+ void exchange(int &m,int &n)
+ {
+     int temp;
+     temp=m;
+     m=n;
+     n=temp;
+}      
